257bd3859c33d6ee8b62c193309543e71ca0c098
Nabil Woalid Rafiu <nabilwoalidrafiu@gmail.com>
2022-11-19 09:42:03 +0600

    leader added

--- script.js
+++ script.js
@@ -7,35 +7,50 @@ const btn2Add3 =document.getElementById('btn2-add3')
 const homeScore = document.getElementById('home-score')
 const guestScore = document.getElementById('guest-score');
 const newGame = document.getElementById('new-game')
+const leaderEL = document.getElementById('leader-el')
 
 btn1Add1.addEventListener('click', function() {
     const add1 = parseInt(btn1Add1.innerText) + parseInt(homeScore.innerText)
     homeScore.innerText = add1
+    leader()
+  
 })
 btn1Add2.addEventListener('click', function() {
     const add2 = parseInt(btn1Add2.innerText) + parseInt(homeScore.innerText)
     homeScore.innerText = add2
+    leader()
 })
 btn1Add3.addEventListener('click', function() {
     const add3 = parseInt(btn1Add3.innerText) + parseInt(homeScore.innerText)
     homeScore.innerText = add3
+    leader()
 })
 
 btn2Add1.addEventListener('click', function() {
     const add21 = parseInt(btn2Add1.innerText) + parseInt(guestScore.innerText)
-    console.log(add21)
     guestScore.innerText = add21
+    leader()
 })
 btn2Add2.addEventListener('click', function() {
     const add22 = parseInt(btn2Add2.innerText) + parseInt(guestScore.innerText)
     guestScore.innerText = add22
+    leader()
 })
 btn2Add3.addEventListener('click', function() {
     const add31 = parseInt(btn2Add3.innerText) + parseInt(guestScore.innerText)
     guestScore.innerText = add31
+    leader()
 })
 
 newGame.addEventListener('click', function(){
     homeScore.innerText = "0"
     guestScore.innerText = "0"
 })
+console.log(parseInt(homeScore.innerText))
+function leader() {
+    if (parseInt(homeScore.innerText) > parseInt(guestScore.innerText)) {
+        leaderEL.innerText = "Home is leading";
+    } else if (parseInt(guestScore.innerText) > parseInt(homeScore.innerText)) {
+        leaderEL.innerText = "Guest is leading";
+    }
+}
